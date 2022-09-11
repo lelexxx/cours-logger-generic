b6a93e76f3ec5f5f8df216b07fd46e4100290b49
lelexxx <rruiz.alex@gmail.com>
2022-09-11 17:56:33 +0200

    Generic method to execute Promise and log result/error

--- js/src/typescript/interfaces/iLogger.ts
+++ js/src/typescript/interfaces/iLogger.ts
@@ -1,5 +1,7 @@
+import { LoggerLevel } from "../enums/loggerLevel";
+
 export default interface ILogger{
-    callAndTrace<TResult>(action: Promise<TResult>): TResult;
+    callAndTrace<TResult>(action: Promise<TResult>): Promise<TResult>;
 
     trace<T>(level: LoggerLevel, message: T): void;
 }
--- js/src/typescript/logger/consoleLogger.ts
+++ js/src/typescript/logger/consoleLogger.ts
@@ -1,22 +1,23 @@
+import { LoggerLevel } from "../enums/loggerLevel";
 import ILogger from "../interfaces/iLogger";
 
 export default class ConsoleLogger implements ILogger{
-    callAndTrace<TResult>(action: Promise<TResult>): TResult{
-        let result: TResult = null;
+    async callAndTrace<TResult>(action: Promise<TResult>): Promise<TResult>{
         try{
-            action.then(r => result = r);
-            this.trace(LoggerLevel.Information, 'action call with success');
+            return await action.then(r => {
+                this.trace(LoggerLevel.Information, 'action call with success');
+
+                if(r === null){
+                    this.trace(LoggerLevel.Warning, 'action has a nullable result');
+                }
+
+                return r;
+            });
         }
         catch{
             this.trace(LoggerLevel.Error, 'action call with Error');
             return null;
         }
-
-        if(result === null){
-            this.trace(LoggerLevel.Warning, 'action has a nullable result');
-        }
-
-        return result;
     }
 
     trace<T>(level: LoggerLevel, message: T): void{
@@ -25,7 +26,7 @@ export default class ConsoleLogger implements ILogger{
                 console.info(message);
                 break;
             case LoggerLevel.Debug :
-                console.debug(message);
+                console.log(message);
                 break;
             case LoggerLevel.Warning :
                 console.warn(message);
