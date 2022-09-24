60f3ec8484d62eb473e8cadd02ed968761cdd11e
Lelexxx <rruiz.alex@gmail.com>
2022-09-24 10:59:46 +0200

    Timing main execution

--- js/src/typescript/App.ts
+++ js/src/typescript/App.ts
@@ -3,26 +3,31 @@ import { LoggerType } from "./enums/loggerType";
 import ILogger from "./interfaces/iLogger";
 import LoggerManager from "./logger/loggerManager";
 import Package from "./model/package";
+import Timer from "./tools/timer";
 
 const logger: ILogger = LoggerManager.getLogger(LoggerType.Console);
 
-const pkgString = new Package<string>("Ceci est un test")
-logger.trace<object>(LoggerLevel.Information, pkgString);
-logger.callAndTrace(pkgString.toString()).then((str) => {
-    logger.trace<string>(LoggerLevel.Debug, str)
+//Utilisation d'un timer pour regrouper les logs d'un même bloc de code ensemble (à cause de l'async)
+Timer.sleep(2000).then(() => {
+    const pkgString = new Package<string>("Ceci est un test");
+    logger.trace(LoggerLevel.Information, "Code n°1");
+    logger.trace<object>(LoggerLevel.Warning, pkgString);
+    logger.callAndTrace(pkgString.toString());
 });
 
-const pkgObject = new Package<object>({id: 1, label: "Ceci est un objet"})
-logger.trace<object>(LoggerLevel.Warning, pkgObject);
-logger.callAndTrace<string>(pkgObject.toString()).then((str) => {
-    logger.trace<string>(LoggerLevel.Debug, str)
-});
-logger.callAndTrace<object>(pkgObject.toJson()).then((json) => {
-    logger.trace<object>(LoggerLevel.Debug, json)
+//Utilisation d'un timer pour regrouper les logs d'un même bloc de code ensemble (à cause de l'async)
+Timer.sleep(2000).then(() => {
+    logger.trace(LoggerLevel.Information, "Code n°2");
+    const pkgObject = new Package<object>({id: 1, label: "Ceci est un objet"});
+    logger.trace<object>(LoggerLevel.Information, pkgObject);
+    logger.callAndTrace<string>(pkgObject.toString());
+    logger.callAndTrace<object>(pkgObject.toJson())
 });
 
-const pkgNull = new Package<null>(null)
-logger.trace<object>(LoggerLevel.Error, pkgNull);
-logger.callAndTrace<string>(pkgNull.toString()).then((json) => {
-    logger.trace<string>(LoggerLevel.Debug, json)
+//Utilisation d'un timer pour regrouper les logs d'un même bloc de code ensemble (à cause de l'async)
+Timer.sleep(2000).then(() => {
+    logger.trace(LoggerLevel.Information, "Code n°3");
+    const pkgNull = new Package<null>(null);
+    logger.trace<object>(LoggerLevel.Error, pkgNull);
+    logger.callAndTrace<string>(pkgNull.toString());
 });
